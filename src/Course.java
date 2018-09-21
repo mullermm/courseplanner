4dc1bd14f95871353f650b0e6cff2f78f45f7f68
mahamedahmed <adenh@augsburg.edu>
2018-09-21 13:36:17 -0500

    Added construcor and getters

--- src/Course.java
+++ src/Course.java
@@ -12,7 +12,50 @@ public class Course {
     String department;              //What department the course is a part of
 
 
+    public Course(String name, int credits, String[] prereq, String coursenumber, Enum offeredIn, String description, String department) {
+        this.name = name;
+        this.credits = credits;
+        this.prereq = prereq;
+        this.coursenumber = coursenumber;
+        OfferedIn = offeredIn;
+        this.description = description;
+        this.department = department;
+    }
 
+
+
+    public Course(String name, String coursenumber, String description, String[] prereq, int credits){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public String[] getPrereq() {
+        return prereq;
+    }
+
+    public String getCoursenumber() {
+        return coursenumber;
+    }
+
+    public Enum getOfferedIn() {
+        return OfferedIn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 
     /**
      * These enums are used by the Course object to specify what semeseters the course is offered.
