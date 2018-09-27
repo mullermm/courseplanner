e42501ef2b7c3d126ee97a747ac21888ff11c0ea
Gebreyesus <beteab@live.com>
2018-09-27 01:28:00 -0500

    Scanner built for words--needs edit

--- src/CourseListTextScanner.java
+++ src/CourseListTextScanner.java
@@ -37,6 +37,27 @@ public class CourseListTextScanner {
 
         RemovePageNumber();
 
+///-----------------added this chuck to read in words. ill edit it to look at lines(tomorow)
+
+     HashMap wordMap = new HashMap();  // Map of word --> # of occurrences
+        int totNumofWords = 0;//counts total num of words
+        //scanner object is instantiated with name Cursor
+        Scanner Cursor = new Scanner(new File("Sample.txt")); //file(Sample.txt) is opened to be read
+
+        while (Cursor.hasNext()) //keep reading until cursor has reached end of document
+        {
+            String Word = Cursor.next();//capturing the next word in text
+            if(wordMap.containsKey(Word)) //if this is another instance of the word we have encountered
+             {
+                // if word instance exists --> increment the number of appearances +1
+                Integer count = (Integer)wordMap.get(Word);
+                wordMap.put(Word, new Integer(count.intValue() + 1));
+             } 
+            else//New word at last -> add to the Map and make occurence=1  
+                wordMap.put(Word, new Integer(1));
+        totNumofWords++;            
+        }
+
 
     }
 
