24e3bc793831439173fd088d0b8c644c01a8e8bb
timlymoua <mouatt@augsburg.edu>
2018-10-15 09:38:58 -0500

    Merge branch 'master' of https://github.com/mullermm/courseplanner into timlyworking
    # Conflicts:
    #	out/production/CoursePlanner/CourseListTest.class
    #	src/CourseListTest.java

---
+++ src/Degree.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Scanner;
+
+public class Degree {
+    public static void main(String[] args) {
+
+        String degreeName;
+
+        String[] CoreRequirements;
+
+        ArrayList<Dictionary(int, String[])>
+        //list of all requirements
+
+
+    }
+}
